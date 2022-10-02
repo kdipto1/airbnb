b64dc8dab7375105b3957ab002c381020dc4a4ff
kdipto1 <kdipto@outlook.com>
2022-10-02 21:26:08 +0600

    header and category setion done

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -10,12 +10,17 @@ const Category = () => {
       .then((data) => setCategory(data));
   },[])
   return (
-    <section className="container mx-auto">
+    <section  className="container mx-auto mt-4">
       <div className="flex flex-wrap">
         {category?.map((c) => (
           <div key={c.id} className="mx-auto m-2">
-            <img className="mx-auto" style={{ width: "24px" }} src={c.image} alt="" />
-            <p className="text-xs">{c.name}</p>
+            <img
+              className="mx-auto"
+              style={{ width: "24px" }}
+              src={c.image}
+              alt=""
+            />
+            <p className="text-xs mt-2">{c.name}</p>
           </div>
         ))}
       </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import airbnb from '../logos/airbnb.svg';
+import avatar from '../logos/avatar.svg';
 const Header = () => {
   return (
-    <div style={{height:"80px", padding:"0 80px"}} className="navbar bg-base-100">
+    <div
+      style={{
+        height: "80px",
+        padding: "0 80px",
+        borderBottom: "1px solid rgb(88,88,88,0.2)",
+      }}
+      className="navbar bg-base-100"
+    >
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -55,18 +63,76 @@ const Header = () => {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl"><img style={{width:"116px"}} src={airbnb} alt="" /></a>
+        <a className="btn btn-ghost normal-case text-xl">
+          <img style={{ width: "116px" }} src={airbnb} alt="" />
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal p-0 ">
-          <p>Anywhere</p>
-          <p>Any week</p>
-          <p>Add guest</p>
-          
+        <ul className="menu menu-horizontal p-3 rounded-badge shadow shadow-slate-200 ">
+          <p className="px-4">Anywhere</p>
+          <div style={{ border: "1px solid grey" }}></div>
+          <p className="px-4">Any week</p>
+          <div style={{ border: "1px solid grey" }}></div>
+          <p className="px-4" style={{ color: "#717172" }}>
+            Add guest
+          </p>
+          <button
+            style={{ borderRadius: "50%", backgroundColor: "#ff385c" }}
+            className="p-1 text-white"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Get started</a>
+        <p className="text-sm font-medium px-7">Become a Host</p>
+        <svg
+          style={{ width: "20px" }}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-6 h-6 "
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+          />
+        </svg>
+        <a className="   shadow-slate-200 px-7">
+          <div style={{padding:"5px 5px 5px 12px"}} className="flex justify-center items-center rounded-badge shadow">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+            {/*  */}
+            <img className="w-7" src={avatar} alt="" />
+          </div>
+        </a>
       </div>
     </div>
   );
