9af4a126b70b57b7473dcb17b223645b14b5e91b
kdipto1 <kdipto@outlook.com>
2022-10-02 15:41:10 +0600

    category section added

---
+++ src/Components/Category.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Category = () => {
+  const [category, setCategory] = useState();
+  useEffect(() => {
+    fetch("/category.json")
+      .then((res) => res.json())
+      .then((data) => setCategory(data));
+  },[])
+  return (
+    <section className="container mx-auto">
+      <div className="flex flex-wrap">
+        {category?.map((c) => (
+          <div key={c.id} className="mx-auto m-2">
+            <img className="mx-auto" style={{ width: "24px" }} src={c.image} alt="" />
+            <p className="text-xs">{c.name}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Category;
